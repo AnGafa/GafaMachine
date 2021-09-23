a0d1e37b42f7bec3166eaf92ea76d3ded82ee6f8
AnGafa <56064109+AnGafa@users.noreply.github.com>
2021-09-23 19:07:29 +0200

    V2

--- httpsRequest.js
+++ httpsRequest.js
@@ -1,35 +1,3 @@
-function getMovieSearch()
-{
-	movies = [];
-	var request = require('request');
-	var options = {
-		'method': 'GET',
-		'url': 'https://imdb-api.com/en/API/SearchMovie/k_iw6gkwfo/inception 2010',
-		'headers': {
-		}
-	};
-	request(options, function (error, response) { 
-		if (error) throw new Error(error)
-
-		searchMovieJSON(JSON.parse(response.body));
-		
-	});
-
-	function searchMovieJSON(response)
-	{
-		parsedResponse = []
-		parsedResponse = response;
-
-		console.log(movies);
-		for(var i = 0; i < parsedResponse.results.length; i ++)
-		{
-			movies[i] = response.results[i];
-		}
-	}
-	console.log(movies);
-	return movies;
-}
-
 const fetch = require("node-fetch");
 
 const getData = async url => {
@@ -56,4 +24,15 @@ async function mainGetMovieData(movieInput)
 	return movies
 }
 
-module.exports = { mainGetMovieData };
+async function getMovieRating(movieInput)
+{
+	const url = "https://imdb-api.com/en/API/Ratings/k_iw6gkwfo/"+movieInput;
+
+	let json = await getData(url);
+
+	let movie = [];
+	movie = json;
+	return movie
+}
+
+module.exports = { mainGetMovieData , getMovieRating};
--- index.js
+++ index.js
@@ -1,276 +1,346 @@
 const Discord = require('discord.js');
-const {prefix, token} = require('./config.json');
-const client = new Discord.Client();
+const { prefix, token } = require('./config.json');
+
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 
 // Import node js fs module.
 var fs = require('fs');
-const httpsRequest = require("./httpsRequest")
+const httpsRequest = require("./httpsRequest");
+const { channel } = require('diagnostics_channel');
 var moviefile = "./movies.json"
 
-const embed = new Discord.MessageEmbed()
+const { MessageEmbed } = require('discord.js');
 
 client.once('ready', () => {
 	console.log('Ready!');
-	client.user.setActivity("!movies commands"); 
+	client.user.setActivity("!movies commands");
 });
 
-client.on('message', async message => {
-	
+client.on('messageCreate', async message => {
+
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	const args = message.content.slice(prefix.length).trim().split(' ');
 	const command = args.shift().toLowerCase();
 
-    if (command === 'creator') {
+	if (command === 'creator') {
 		message.channel.send('Andrea Gafa');
 	}
 
-	if (command === 'movies')
-	{
-		if(args[0] === "horror")
-		{
+	if (command === 'movies') {
+		if (args[0] === "horror") {
 			var parselist = [];
 			parselist = readjsonfile();
-			
-			let data = parselist.movies.categories.horror;
-			var movielist ="";
 
-			for(i=0; i<data.length; i++)
-			{
-				movielist = movielist.concat(data[i].id,".\t", data[i].name, "\n");
+			let data = parselist.movies.categories.horror;
+			var movielist = "";
+
+			for (i = 0; i < data.length; i++) {
+				movielist = movielist.concat(data[i].id, ".\t", data[i].name, "\n");
 			}
 
 			embedmsg("horror", movielist, message);
-		}else if(args[0] === "comedy")
-		{
+		} else if (args[0] === "comedy") {
 			var parselist = [];
 			parselist = readjsonfile();
-			
-			let data = parselist.movies.categories.comedy;
-			var movielist ="";
 
-			for(i=0; i<data.length; i++)
-			{
-				movielist = movielist.concat(data[i].id,".\t", data[i].name, "\n");
+			let data = parselist.movies.categories.comedy;
+			var movielist = "";
+
+			for (i = 0; i < data.length; i++) {
+				movielist = movielist.concat(data[i].id, ".\t", data[i].name, "\n");
 			}
 
 			embedmsg("comedy", movielist, message);
 
-		}else if(args[0] === "action")
-		{
+		} else if (args[0] === "action") {
 			var parselist = [];
 			parselist = readjsonfile();
-			
-			let data = parselist.movies.categories.action;
-			var movielist ="";
 
-			for(i=0; i<data.length; i++)
-			{
-				movielist = movielist.concat(data[i].id,".\t", data[i].name, "\n");
+			let data = parselist.movies.categories.action;
+			var movielist = "";
+
+			for (i = 0; i < data.length; i++) {
+				movielist = movielist.concat(data[i].id, ".\t", data[i].name, "\n");
 			}
 
 			embedmsg("action", movielist, message);
 
-		}else if(args[0] === undefined)
-		{		
+		} else if (args[0] === undefined) {
 			var parselist = [];
 			parselist = readjsonfile();
-			
+
 			var horrorMovielist = "";
 			var comedyMovielist = "";
 			var actionMovielist = "";
-			
-			for(i=0; i<parselist.movies.categories.horror.length; i++)
-			{
-				horrorMovielist = horrorMovielist.concat(parselist.movies.categories.horror[i].id,".\t", parselist.movies.categories.horror[i].name, "\n");
-			}
-			
-			for(i=0; i<parselist.movies.categories.comedy.length; i++)
-			{
-				comedyMovielist = comedyMovielist.concat(parselist.movies.categories.comedy[i].id,".\t", parselist.movies.categories.comedy[i].name, "\n");
-			}
-			
-			for(i=0; i<parselist.movies.categories.action.length; i++)
-			{
-				actionMovielist = actionMovielist.concat(parselist.movies.categories.action[i].id,".\t", parselist.movies.categories.action[i].name, "\n");
+
+			for (i = 0; i < parselist.movies.categories.horror.length; i++) {
+				horrorMovielist = horrorMovielist.concat(parselist.movies.categories.horror[i].id, ".\t", parselist.movies.categories.horror[i].name, "\n");
 			}
 
-			message.channel.send({embed: {
-				color: '#D733FF',
-				title: 'movie list',
-				fields: [
-					{
-						name: 'HORROR/THRILLER',
-						value: horrorMovielist,
-						inline: true,
-					},
-					{
-						name: 'COMEDY',
-						value: comedyMovielist,
-						inline: true,
-					},
-					{
-						name: 'ACTION',
-						value: actionMovielist,
-						inline: true,
-					},
-				],
-				timestamp: new Date(),
-				footer: {
-					icon_url: client.user.displayAvatarURL(),
-					text: 'Andrea Gafa'
-				}
-			  }})
+			for (i = 0; i < parselist.movies.categories.comedy.length; i++) {
+				comedyMovielist = comedyMovielist.concat(parselist.movies.categories.comedy[i].id, ".\t", parselist.movies.categories.comedy[i].name, "\n");
+			}
+
+			for (i = 0; i < parselist.movies.categories.action.length; i++) {
+				actionMovielist = actionMovielist.concat(parselist.movies.categories.action[i].id, ".\t", parselist.movies.categories.action[i].name, "\n");
+			}
+
+			let embed = new MessageEmbed()
+				.setColor('#D733FF')
+				.setTitle('movie list')
+				.addFields({
+					name: 'HORROR/THRILLER',
+					value: horrorMovielist,
+					inline: true,
+				},
+				{
+					name: 'COMEDY',
+					value: comedyMovielist,
+					inline: true,
+				},
+				{
+					name: 'ACTION',
+					value: actionMovielist,
+					inline: true,
+				})
+				.setTimestamp(new Date())
+				.setFooter(text = "By Andrea Gafa'")
+
+			message.channel.send({embeds: [embed]})
 				.then(msg => {
 					setTimeout(() => msg.delete(), 300000)
 				})
 				.catch(/*Your Error handling if the Message isn't returned, sent, etc.*/);
 
-		}else if(args[0] === "seen")
-		{
+		} else if (args[0] === "seen") {
 			var parselist = [];
 			parselist = readjsonfile();
-			
-			let data = parselist.seen;
-			var movielist ="";
 
-			for(i=0; i<data.length; i++)
-			{
+			let data = parselist.seen;
+			var movielist = "";
+
+			for (i = 0; i < data.length; i++) {
 				movielist = movielist.concat(data[i].name, "\n");
 			}
 			embedmsg("seen", movielist, message);
-			
-		}else if(args[0] === "add")
-		{
-			if(args[1] === "horror")
-			{
+
+		} else if (args[0] === "add") {
+			if (args[1] === "horror") {
 				writejsonfile("horror", args.slice(2).join(' '));
 				message.react('👍');
-			}else if(args[1] === "comedy")
-			{
+			} else if (args[1] === "comedy") {
 				writejsonfile("comedy", args.slice(2).join(' '));
 				message.react('👍');
-			}else if(args[1] === 'action')
-			{
+			} else if (args[1] === 'action') {
 				writejsonfile("action", args.slice(2).join(' '));
 				message.react('👍');
-			}else if(args[1] === 'seen')
-			{
+			} else if (args[1] === 'seen') {
 				movetoseen(args[2], message)
 			}
-		}else if((args[0] === "commands") || (args[0] === "cmd"))
-		{
-			message.channel.send({embed: {
-				color: '#D733FF',
-				title: 'commands list',
-				description: '!movies\n!movies [category]\n!movies seen\n!movies add [category] [movie name]\n !movies add seen [movie id]',
-				timestamp: new Date(),
-				footer: {
-					icon_url: client.user.displayAvatarURL(),
-					text: 'Andrea Gafa'
-				}
-			  }})
+		} else if ((args[0] === "commands") || (args[0] === "cmd")) {
+			let embed = new MessageEmbed()
+				.setColor('#D733FF')
+				.setTitle("commands list")
+				.setDescription("!movies\n!movies [category]\n!movies seen\n!movies add [category] [movie name]\n !movies add seen [movie id]")
+				.setTimestamp(new Date())
+				.setFooter(text = "By Andrea Gafa'")
+
+			message.channel.send({ embeds: [embed] })
 				.then(msg => {
 					setTimeout(() => msg.delete(), 300000)
 				})
 				.catch(/*Your Error handling if the Message isn't returned, sent, etc.*/);
-		}else if(args[0] === "check")
-		{
+		} else if (args[0] === "check") {
 			let movies = await httpsRequest.mainGetMovieData(args.slice(1).join(' '));
 
 			let count = 0;
-			if(movies.length % 4 !== 0)
-			{
-				i = movies.length+1;
-				b = {title:null , description: null};
+			if (movies.length % 4 !== 0) {
+				i = movies.length + 1;
+				b = { title: "N/A", description: "N/A" };
 
-				while(movies.length % 4 !== 0)
-				{
+				while (movies.length % 4 !== 0) {
 					movies.push(b)
 					i++;
 				}
 			}
-
 			movieChoiceEmbed(movies, message, count);
+
+			client.on("messageReactionAdd", async (reaction, user) => { // When a reaction is added
+				if (user.bot) return;
+		
+				if (reaction.emoji.name == "⏪") {
+					await reaction.message.delete()
+						.then()
+						.catch(err => { console.error(err); });
+					if (count >= 4) {
+						count -= 4;
+					}
+					movieChoiceEmbed(movies, message, count);
+				} else if (reaction.emoji.name == "⏩") {
+					await reaction.message.delete()
+						.then()
+						.catch(err => { console.error(err); });
+					if (count < (movies.length-4)) {
+						count += 4;
+					}
+					movieChoiceEmbed(movies, message, count);
+				}else if (reaction.emoji.name == "❌") {
+					await reaction.message.delete()
+						.then()
+						.catch(err => { console.error(err); });
+					return;
+				}else if (reaction.emoji.name == "1️⃣") {
+					await reaction.message.delete()
+						.then()
+						.catch(err => { console.error(err); });
+					let selectedInfo = movies[count];
+					selectedMovieEmbed(selectedInfo, message)
+				}else if (reaction.emoji.name == "2️⃣") {
+					await reaction.message.delete()
+						.then()
+						.catch(err => { console.error(err); });
+					let selectedInfo = movies[count+1];
+					selectedMovieEmbed(selectedInfo, message)
+				}else if (reaction.emoji.name == "3️⃣") {
+					await reaction.message.delete()
+						.then()
+						.catch(err => { console.error(err); });
+					
+					let selectedInfo = movies[count+2];
+					selectedMovieEmbed(selectedInfo, message)
+				}else if (reaction.emoji.name == "4️⃣") {
+					await reaction.message.delete()
+						.then()
+						.catch(err => { console.error(err); });
+					
+					let selectedInfo = movies[count+3];
+					selectedMovieEmbed(selectedInfo, message)
+				}
+				return;
+			});
+		} else if (args[0] === "test") {
+
+			let embed2 = new MessageEmbed()
+					.setColor('#0099ff')
+					.setTitle('Some title')
+					.setURL('https://discord.js.org/')
+
+			test();
+
+			async function test() {
+
+				var mg = await message.channel.send({ embeds: [embed2] });
+
+				mg.react("⏪");
+				mg.react("⏩");
+				message.channel.send("a");
+				
+			}
+
+			client.on("messageReactionAdd", async (reaction, user) => { // When a reaction is added					
+				if (user.bot) return;
+				message.channel.send("b");
+				if (reaction.emoji.name == "⏪") {
+					await reaction.message.delete()
+					return;
+				} else if (reaction.emoji.name == "⏩") {
+					message.channel.send("c");
+					await reaction.message.delete()
+					test();
+				}
+				return;
+			});
+
 		}
 	}
 })
 
-function movieChoiceEmbed(movies, message, count)
-{
+async function movieChoiceEmbed(movies, message, count) {
 	pagecount = count;
 
-	message.channel.send({embed: {
-		color: '#D733FF',
-		title: 'Choose movie',
-		fields: [
-			{
-				name: movies[pagecount].title,
-				value: movies[pagecount].description,
-				inline: true,
-			},
-			{
-				name: movies[pagecount+1].title,
-				value: movies[pagecount+1].description,
-				inline: true,
-			},
-			{
-				name: '\u200b',
-				value: '\u200b',
-				inline: false,
-			},
-			{
-				name: movies[pagecount+2].title,
-				value: movies[pagecount+2].description,
-				inline: true,
-			},
-			{
-				name: movies[pagecount+3].title,
-				value: movies[pagecount+3].description,
-				inline: true,
-			},
-		],
-		timestamp: new Date(),
-		footer: {
-			icon_url: client.user.displayAvatarURL(),
-			text: 'Andrea Gafa'
-		}
-	  }})
+	let embed = new MessageEmbed()
+		.setColor('#D733FF')
+		.setTitle('Choose movie')
+		.addFields({
+			name: movies[pagecount].title,
+			value: movies[pagecount].description,
+			inline: true,
+		},
+		{
+			name: movies[pagecount + 1].title,
+			value: movies[pagecount + 1].description,
+			inline: true,
+		},
+		{
+			name: '\u200b',
+			value: '\u200b',
+			inline: false,
+		},
+		{
+			name: movies[pagecount + 2].title,
+			value: movies[pagecount + 2].description,
+			inline: true,
+		},
+		{
+			name: movies[pagecount + 3].title,
+			value: movies[pagecount + 3].description,
+			inline: true,
+		})
+		.setTimestamp(new Date())
+		.setFooter(text = "By Andrea Gafa'")
+
+	var mg = await message.channel.send({ embeds: [embed] })
 		.then(sentEmbed => {
 			sentEmbed.react("⏪");
+			sentEmbed.react("❌");
 			sentEmbed.react("⏩");
+			sentEmbed.react("1️⃣");
+			sentEmbed.react("2️⃣");
+			sentEmbed.react("3️⃣");
+			sentEmbed.react("4️⃣");
 		})
 		.catch();
-
-	client.on("messageReactionAdd", async (reaction, user) => { // When a reaction is added
-		if(user.bot) return; 
-
-		if(reaction.emoji.name == "⏪")
-		{
-			await reaction.message.delete()
-				.then()
-				.catch(err => { console.error(err); });
-			if(count>4)
-			{
-				count -= 4;
-			}
-			movieChoiceEmbed(movies, message, count);
-		}else if(reaction.emoji.name == "⏩")
-		{
-			await reaction.message.delete()
-				.then()
-				.catch(err => { console.error(err); });
-			if(count<movies.length)
-			{
-				count += 4;
-			}
-			movieChoiceEmbed(movies, message, count);
-		}
-		return;
-	});
 }
 
-function readjsonfile()
-{
+async function selectedMovieEmbed(selectedInfo, message){
+	let movie = await httpsRequest.getMovieRating(selectedInfo.id);	
+
+	if(movie.fullTitle == "") 
+		movie.fullTitle = "N/A";
+	if(movie.imDb == "")
+		movie.imDb = "N/A"
+	if(movie.metacritic == "")
+		movie.metacritic = "N/A"
+	if(movie.theMovieDb == "")
+		movie.theMovieDb = "N/A"
+	if(movie.rottenTomatoes == "")
+		movie.rottenTomatoes = "N/A"
+	if(movie.filmAffinity == "")
+		movie.filmAffinity = "N/A"
+	if(selectedInfo.image == "")
+		selectedInfo.image = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png"
+
+	let embed = new MessageEmbed()
+		.setColor('#D733FF')
+		.setTitle(movie.fullTitle)
+		.addField("IMDB: ", movie.imDb)
+		.addField("Metacritic: ", movie.metacritic)
+		.addField("theMovieDb: ", movie.theMovieDb)
+		.addField("rottenTomatoes: ", movie.rottenTomatoes)
+		.addField("filmAffinity: ", movie.filmAffinity)
+		.setImage(selectedInfo.image)
+		.setTimestamp(new Date())
+		.setFooter(text = "By Andrea Gafa'")
+
+	var mg = await message.channel.send({ embeds: [embed] })
+	.then(msg => {
+		setTimeout(() => msg.delete(), 300000)
+	})
+	.catch(/*Your Error handling if the Message isn't returned, sent, etc.*/);
+}
+
+function readjsonfile() {
 	const fs = require('fs');
 	const path = require('path');
 
@@ -279,47 +349,44 @@ function readjsonfile()
 	return movielist;
 }
 
-function writejsonfile(category, movie)
-{
+function writejsonfile(category, movie) {
 	var fs = require('fs');
-	var obj = {movies: {categories: {horror:[],comedy:[],action:[]}}};
+	var obj = { movies: { categories: { horror: [], comedy: [], action: [] } } };
 
-	fs.readFile('movies.json', 'utf8', function readFileCallback(err, data){
-		if (err){
+	fs.readFile('movies.json', 'utf8', function readFileCallback(err, data) {
+		if (err) {
 			console.log(err);
 		} else {
-		obj = JSON.parse(data); //now its an object
-		var id = obj.id;
-		obj.movies.categories[category].push({id: id, name: movie.toUpperCase()}); //add some data
-		obj.id = id+1;
-		json = JSON.stringify(obj); //convert it back to json
-		fs.writeFile('movies.json', json, (err) => {
-			if (err) throw err;
-			console.log('movie file overwritten');
-		  });
-	}});
+			obj = JSON.parse(data); //now its an object
+			var id = obj.id;
+			obj.movies.categories[category].push({ id: id, name: movie.toUpperCase() }); //add some data
+			obj.id = id + 1;
+			json = JSON.stringify(obj); //convert it back to json
+			fs.writeFile('movies.json', json, (err) => {
+				if (err) throw err;
+				console.log('movie file overwritten');
+			});
+		}
+	});
 }
 
-function embedmsg(category, movielist, message)
-{
-	message.channel.send({embed: {
-		color: '#D733FF',
-		title: category+' movie list',
-		description: movielist,
-		timestamp: new Date(),
-		footer: {
-			icon_url: client.user.displayAvatarURL(),
-			text: 'Andrea Gafa'
-		}
-	  }})
+async function embedmsg(category, movielist, message) {
+
+	let embed = new MessageEmbed()
+		.setColor('#D733FF')
+		.setTitle(category + ' movie list')
+		.setDescription(movielist)
+		.setTimestamp(new Date())
+		.setFooter(text = "By Andrea Gafa'")
+
+		var mg = await message.channel.send({ embeds: [embed] })
 		.then(msg => {
 			setTimeout(() => msg.delete(), 300000)
 		})
 		.catch(/*Your Error handling if the Message isn't returned, sent, etc.*/);
 }
 
-function movetoseen(id, message)
-{
+function movetoseen(id, message) {
 	var parsedJSON = [];
 	parsedJSON = readjsonfile();
 
@@ -328,41 +395,40 @@ function movetoseen(id, message)
 	actionfilter = parsedJSON.movies.categories.action;
 	var seenEntry;
 
-	for(var i = 0; i < horrorfilter.length; i++) {
-		if(horrorfilter[i].id == id) {
+	for (var i = 0; i < horrorfilter.length; i++) {
+		if (horrorfilter[i].id == id) {
 			seenEntry = horrorfilter[i];
 			horrorfilter.splice(i, 1);
 			break;
 		}
 	}
-	if (seenEntry == null){
-		for(var i = 0; i < comedyfilter.length; i++) {
-			if(comedyfilter[i].id == id) {
+	if (seenEntry == null) {
+		for (var i = 0; i < comedyfilter.length; i++) {
+			if (comedyfilter[i].id == id) {
 				seenEntry = comedyfilter[i];
 				comedyfilter.splice(i, 1);
 				break;
 			}
 		}
 	}
-	if (seenEntry == null){
-		for(var i = 0; i < actionfilter.length; i++) {
-			if(actionfilter[i].id == id) {
+	if (seenEntry == null) {
+		for (var i = 0; i < actionfilter.length; i++) {
+			if (actionfilter[i].id == id) {
 				seenEntry = actionfilter[i];
 				actionfilter.splice(i, 1);
 				break;
 			}
 		}
 	}
-	if (seenEntry == null)
-	{
+	if (seenEntry == null) {
 		message.react('👎');
-	}else{
+	} else {
 		parsedJSON.seen.push(seenEntry);
 		json = JSON.stringify(parsedJSON); //convert it back to json
 		fs.writeFile('movies.json', json, (err) => {
 			if (err) throw err;
 			console.log('movie file overwritten');
-			});
+		});
 
 		message.react('👍');
 	}
